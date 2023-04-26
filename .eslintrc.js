9924f900bb7f76c125cb7ed59e95edff0f5daa85
alexmyt <alexmyt1@yandex.ru>
2023-04-26 11:55:27 +0300

    chore: config es-lint, add prettier, add '/src' folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,38 +4,38 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-typescript/base',
+        "plugin:@typescript-eslint/recommended",
+        "plugin:jest/recommended",
+        "airbnb-typescript/base",
+        "prettier",
       ],
       parserOptions: {
-        project: './tsconfig.json',
+        project: "./tsconfig.json",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'import/extensions': 'off',
+    "import/extensions": "off",
+    "linebreak-style": ["error", "unix"],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-function-return-type": ["warn"],
+    "prettier/prettier": ["error"],
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
